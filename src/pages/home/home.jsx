6baf9ec56aad8bede6b3952baad2324319319046
ms.moradi <ms.moradi28@gmail.com>
2023-05-15 08:08:09 +0430

    add elements and styles

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -14,6 +14,37 @@ import KeyboardDoubleArrowDownIcon from '@mui/icons-material/KeyboardDoubleArrow
 const Home =()=>{
     return(
         <>
+            <DialogSearch open={open} setOpen={setOpen} coins={coins} selectFees={selectFees} setSelectFees={setSelectFees} setTextField={setTextField} textField={textField}/>
+            <Grid sx={{display:{ xs:'none' , md:'block'}, marginBottom:'200px', marginTop:'80px'}}>
+                <Grid container sx={{justifyContent:'center', marginTop:'20px'}}>
+                    <img src={Photo} alt="" style={{width:'250px',height:'88px'}}></img>
+                </Grid>
+                <Grid container sx={{justifyContent:'center' , gap:'2px',marginTop:'8px',marginBottom:'18px'}} >
+                    <StyledTextField label="دلار" type="number" name="Toman" value={(Number(textField.Toman)*unit).toFixed(3)} />
+                    <StyledTextField label="واحد" type="number" name="unit" value={unit} onChange={handleChangeUnit}/>
+                    <StyledTextField label="انتخاب ارز"  type="text" name="name"  value={textField.name} onClick={handleClickOpen}/>
+                </Grid>
+                <Grid container sx={{justifyContent:'center',gap:'5px'}}>
+                    <StyledButton sx={{width:'140px', height:'35px'}}>
+                        <Typography>درخواست خرید</Typography>
+                    </StyledButton>
+                    <StyledButton sx={{width:'140px', height:'35px'}}>
+                        <Typography>فروش</Typography>
+                    </StyledButton>
+                </Grid>
+                <Grid container sx={{flexDirection:'column', alignItems:'center', marginTop:'15px'}}>
+                    <Typography>پشتیبانی ۲۴ ساعته</Typography>
+                    <Typography>حتی در روزهای تعطیل</Typography>
+                    <Grid container item sx={{justifyContent:'center', }}>
+                        <Typography color="primary">
+                            ۰۲۱-۹۱۰۷۹۶۷۷
+                        </Typography>
+                        <PhoneIcon color="primary"/>
+                    </Grid>
+                    <KeyboardDoubleArrowDownIcon color='primary' sx={{fontSize:50, transition:{easing:{easeInOut: 'cubic-bezier(0.4, 0, 0.2, 1)'}}}}/>
+                </Grid>
+            </Grid>
+            <MainTable coins={coins}/>
         </>
     )
 }
