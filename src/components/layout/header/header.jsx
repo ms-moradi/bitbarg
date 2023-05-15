c415841e18e76425913741058b9fdac167c22e62
ms.moradi <ms.moradi28@gmail.com>
2023-05-15 07:56:09 +0430

    add imports

--- src/components/layout/header/header.jsx
+++ src/components/layout/header/header.jsx
@@ -1,4 +1,22 @@
-
+import React, {useState} from "react";
+import { Link } from "react-router-dom";
+import StyledButton from "../../../styles/styleButton";
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import Grid from "@mui/material/Grid";
+import Button from "@mui/material/Button";
+import Backdrop from '@mui/material/Backdrop';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import Fade from '@mui/material/Fade';
+import TextField from '@mui/material/TextField';
+import Drawer from '@mui/material/Drawer';
+import logo from 'assets/image/header-logo.svg';
+import CloseIcon from '@mui/icons-material/Close';
+import HomeIcon from '@mui/icons-material/Home';
+import WalletIcon from '@mui/icons-material/Wallet';
+import InsertChartIcon from '@mui/icons-material/InsertChart';
 
 const Header =()=>{
     return(
