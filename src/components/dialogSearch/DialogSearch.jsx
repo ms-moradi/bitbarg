c87896ac1d67cbe11b80a48c83e4a608d5b2e8af
ms.moradi <ms.moradi28@gmail.com>
2023-05-15 10:48:07 +0430

    add imports

--- src/components/dialogSearch/DialogSearch.jsx
+++ src/components/dialogSearch/DialogSearch.jsx
@@ -1,8 +1,22 @@
-
+import {useState} from "react";
+import Dialog from "@mui/material/Dialog";
+import DialogTitle from "@mui/material/DialogTitle";
+import DialogContent from "@mui/material/DialogContent";
+import InputAdornment from '@mui/material/InputAdornment';
+import TextField from '@mui/material/TextField';
+import Grid from "@mui/material/Grid";
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableRow from '@mui/material/TableRow';
+import Typography from '@mui/material/Typography';
+import SearchIcon from '@mui/icons-material/Search';
 
 const DialogSearch =()=>{
     return(
         <>
+
         </>
     )
 }
