f28220d16595099204ac0a78d1fee7bccdf0861e
ms.moradi <ms.moradi28@gmail.com>
2023-05-15 10:54:56 +0430

    add elements and styles

--- src/components/item/item.jsx
+++ src/components/item/item.jsx
@@ -4,8 +4,12 @@ import Typography from "@mui/material/Typography";
 
 const Item =()=>{
     return(
-        <>
-        </>
+        <Grid container item sx={{display:'flex' , flexDirection:'column', justifyContent:'space-evenly',width:'25%' , alignContent:'baseline',padding:'0 10px'}} >
+            {children}
+            <Typography sx={{color:'grey',textAlign:'center'}}>
+                {nameIcon}
+            </Typography>
+        </Grid>
     )
 }
 
