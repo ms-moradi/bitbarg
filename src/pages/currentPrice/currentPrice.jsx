6b777c942a27ce62a9db4528317f35f9589269a3
ms.moradi <ms.moradi28@gmail.com>
2023-05-15 08:11:14 +0430

    add imports

--- src/pages/currentPrice/currentPrice.jsx
+++ src/pages/currentPrice/currentPrice.jsx
@@ -1,4 +1,18 @@
-
+import React,{useState, useEffect} from "react";
+import {handleGetDataCoins} from "../../api/api";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import SearchIcon from '@mui/icons-material/Search';
+import StarBorderIcon from '@mui/icons-material/StarBorder';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import TextField from "@mui/material/TextField";
+import StyledTextField from "../../styles/styleTextField";
+import InputAdornment from '@mui/material/InputAdornment';
 
 
 const CurrentPrice =()=>{
