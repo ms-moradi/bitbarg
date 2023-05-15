24431afe06398860d8ad0099c65d2778a43357b1
ms.moradi <ms.moradi28@gmail.com>
2023-05-15 10:49:19 +0430

    add elements and styles

--- src/components/dialogSearch/DialogSearch.jsx
+++ src/components/dialogSearch/DialogSearch.jsx
@@ -15,9 +15,41 @@ import SearchIcon from '@mui/icons-material/Search';
 
 const DialogSearch =()=>{
     return(
-        <>
-
-        </>
+        <Dialog open={open} onClose={handleClose}>
+            <DialogTitle>انتخاب ارز</DialogTitle>
+            <DialogContent>
+                <Grid container item >
+                    <TextField sx={{width:'100%', height:'35px'}} id="search" onChange={handleSearchFees} size="small"
+                    InputProps={{
+                        startAdornment:(
+                            <InputAdornment position='start'>
+                                <SearchIcon/>
+                            </InputAdornment>
+                        ) 
+                    }}
+                    variant='outlined'
+                    />
+                </Grid>
+                <TableContainer  mt={6}>
+                    <Table sx={{width:'400px', height:'550px'}}>
+                        <TableBody>
+                            {coins.filter(coin => searchFees === '' ? coin : coin.name.toLowerCase().includes(searchFees.toLowerCase()))
+                            .map((coin) => (
+                                <TableRow  key={coin.id} sx={{width:'380px',height:'60px',display:'flex', justifyContent:'space-between'}} onClick={()=>handleSelectFees(coin.id)}>
+                                    <TableCell align='right' sx={{width:'50%', display:'flex', justifyContent:'flex-end', alignItems:'center', gap:'10px', cursor:'pointer'}}>{coin.name}<img src={coin.image} alt="logo" style={{width:'25px', height:'25px'}}/></TableCell>
+                                    <TableCell align='right' sx={{width:'50%', display:'flex', flexDirection:'column',alignItems:'center', gap:'10px'}}>
+                                        <Grid>
+                                            <Typography>قیمت خرید</Typography>
+                                            <Typography>{coin.current_price}</Typography>
+                                        </Grid>
+                                    </TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </DialogContent>
+        </Dialog>
     )
 }
 
