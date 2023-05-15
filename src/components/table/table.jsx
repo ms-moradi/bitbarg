8a1622f11baf2e4872c4817a37649e01cbb85f20
ms.moradi <ms.moradi28@gmail.com>
2023-05-15 10:57:30 +0430

    add imports

--- src/components/table/table.jsx
+++ src/components/table/table.jsx
@@ -1,3 +1,18 @@
+import {Link} from 'react-router-dom';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Grid from '@mui/material/Grid';
+import InsertChartOutlinedIcon from '@mui/icons-material/InsertChartOutlined';
+import Typography from '@mui/material/Typography';
+import TrendingDownIcon from '@mui/icons-material/TrendingDown';
+import TrendingUpIcon from '@mui/icons-material/TrendingUp';
+import Card from '../Card/Card';
+import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 
 
 const MainTable =()=>{
