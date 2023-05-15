bdc5e5708ca5b8a8738e3e85b7e4afe1196c1453
ms.moradi <ms.moradi28@gmail.com>
2023-05-15 08:02:57 +0430

    add imports

--- src/components/layout/footer/footer.jsx
+++ src/components/layout/footer/footer.jsx
@@ -1,7 +1,15 @@
+import Item from "../../Item/Item";
+import Grid from "@mui/material/Grid";
+import HomeIcon from '@mui/icons-material/Home';
+import WalletIcon from '@mui/icons-material/Wallet';
+import ListAltIcon from '@mui/icons-material/ListAlt';
+import PermIdentityIcon from '@mui/icons-material/PermIdentity';
+
 
 const Footer =()=>{
     return(
         <>
+        
         </>
     )
 }
