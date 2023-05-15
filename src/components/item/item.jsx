59e7c7251e8e7c7cf73546dc13c1f4cedc09dcc8
ms.moradi <ms.moradi28@gmail.com>
2023-05-15 10:54:28 +0430

    create file

---
+++ src/components/item/item.jsx
@@ -0,0 +1,12 @@
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+
+
+const Item =()=>{
+    return(
+        <>
+        </>
+    )
+}
+
+export default Item;
