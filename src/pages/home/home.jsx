c85ed097f59d7a842a71c7348229c39d267459c5
ms.moradi <ms.moradi28@gmail.com>
2023-05-15 08:07:14 +0430

    add imports

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,3 +1,14 @@
+import {useState, useEffect} from "react";
+import MainTable from "../../component/Table/Table";
+import DialogSearch from "../../component/DialogSearch/DialogSearch";
+import {handleGetDataCoins} from "../../api/api";
+import Grid from "@mui/material/Grid";
+import Photo from "assets/image/PhotoBitBarg.png";
+import StyledButton from "styles/styleButton";
+import Typography from "@mui/material/Typography";
+import StyledTextField from "../../styles/styleTextField";
+import PhoneIcon from '@mui/icons-material/Phone';
+import KeyboardDoubleArrowDownIcon from '@mui/icons-material/KeyboardDoubleArrowDown';
 
 
 const Home =()=>{
