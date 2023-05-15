ee31a3183986b47dab50f4b462177e56afd12808
ms.moradi <ms.moradi28@gmail.com>
2023-05-15 08:03:53 +0430

    add elements and styles

--- src/components/layout/footer/footer.jsx
+++ src/components/layout/footer/footer.jsx
@@ -8,9 +8,20 @@ import PermIdentityIcon from '@mui/icons-material/PermIdentity';
 
 const Footer =()=>{
     return(
-        <>
-        
-        </>
+        <Grid container bgcolor={'#fff'} sx={{display:{xs:'flex' , md: 'none'} , justifyContent:'space-between',alignItems:'center',width:"100%", position:'fixed', bottom:'0', borderTop:'1px solid grey', padding:'10px 0'}}>
+            <Item nameIcon={"خانه"}>
+                <HomeIcon sx={{color:'grey' , fontSize:30}}/>
+            </Item>
+            <Item nameIcon={"کیف پول "}>
+                <WalletIcon sx={{color:'grey' , fontSize:30}}/>
+            </Item>
+            <Item nameIcon={"سفارشات"}>
+                <ListAltIcon sx={{color:'grey' , fontSize:30}}/>
+            </Item>
+            <Item nameIcon={"پروفایل"}>
+                <PermIdentityIcon sx={{color:'grey' , fontSize:30}}/>
+            </Item>
+        </Grid>
     )
 }
 
