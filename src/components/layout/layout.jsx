8b77f1a939c8d36a583ff462f8d2750e81c3827f
ms.moradi <ms.moradi28@gmail.com>
2023-05-15 07:54:30 +0430

    add imports and create structure layout

--- src/components/layout/layout.jsx
+++ src/components/layout/layout.jsx
@@ -1,9 +1,16 @@
-
+import { Outlet } from "react-router-dom";
+import Header from "./header/header";
+import Footer from "./footer/footer";
 
 const Layout =()=>{
     return(
-        <>
-        </>
+        <div>
+            <Header/>
+            <main>
+                <Outlet/>
+            </main>
+            <Footer/>
+        </div>
     )
 }
 
